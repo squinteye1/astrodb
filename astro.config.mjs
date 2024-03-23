47a4fa43d55bd144617bfa7e497a7d56007f7475
Neil McKay <neil.d.mckay@googlemail.com>
2024-03-23 19:38:58 +0000

    added a build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import db from "@astrojs/db";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   output: 'server',
-  integrations: [mdx(), sitemap(), db()]
+  integrations: [mdx(), sitemap(), db()],
+  adapter: node({
+    mode: "standalone"
+  })
 });
